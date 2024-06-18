c916752ba07f8e09521cdc073b10d36152b11aa3
linalaimik <linalaimik@yandex.ru>
2024-06-18 19:39:20 +0300

    Небольшая помощь

--- git/import_file.py
+++ git/import_file.py
@@ -1,11 +1,40 @@
 import pandas as pd
+import numpy as np
 
 def read_file():
     df_tires = pd.read_excel('data_read/tyres.xlsx')
     print(df_tires)
 
 
-
-
+def analize_file():
+    """Взято из наших предыдущих занятий,
+    можно будет внутри элифов наделать еще выборов, если потребуется
+    надеюсь, что это чем-то поможет =))) С любовью от Лины"""
+    spr = '#' * 38
+    print(spr, ' Вас приветствует служба работы с БД! ', spr, sep='\n')
+    while True:
+        print('Выберите действие: \n\t'
+              '1 - Вывести БД\n\t'
+              '2 - Добавить информацию\n\t'
+              '3 - Удалить информацию\n\t'
+              '4 - Изменить информацию\n\t'
+              '5 - Выйти из программы')
+        print(spr)
+        action = input()
+        print(spr)
+        if action == '1':
+            print_data_base()
+        elif action == '2':
+            input(' Выберите информацию, которую хотите добавить ')
+        elif action == '3':
+            input(' Что Вы хотите удалить? ')
+        elif action == '4':
+            print(' Что Вам нужно изменить? =) ')
+        else:
+            action == '5'
+            print()
+            print(' До новых встреч ')
+            print()
+            break
 if __name__ == '__main__':
-    read_file()
+    analize_file()
