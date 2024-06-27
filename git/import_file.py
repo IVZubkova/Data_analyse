92904a6d0a93682a25448852dddef3c5316467b0
IVZubkova <irtanz@gmail.com>
2024-06-27 10:11:39 +0300

    get diff

--- git/import_file.py
+++ git/import_file.py
@@ -5,6 +5,19 @@ def read_file():
     print(df_tires)
 
 
+def find_diff():
+
+    df1 = pd.read_excel('data_read/kub2.xlsx')
+    df2 = pd.read_excel('data_read/file.xlsx')
+
+    df1['Контрагент RN'] = df1['Контрагент RN'].fillna(0).astype('int64')
+    df2['Контрагент RN'] = df2['Контрагент RN'].fillna(0).astype('int64')
+
+    merged = pd.merge(df1, df2, on='Контрагент RN', how='outer', indicator=True)
+    not_found = merged[merged['_merge'] != 'both']
+
+    not_found.to_excel('различия.xlsx', index=False)
+
 
 
 if __name__ == '__main__':
