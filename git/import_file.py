b4101fb228d9b3ff25b18b5c8cfdde06ef10b708
IVZubkova <irtanz@gmail.com>
2024-07-16 09:44:38 +0300

    add function find word

--- git/import_file.py
+++ git/import_file.py
@@ -1,8 +1,37 @@
 import pandas as pd
+import openpyxl
 
 def read_file():
     df_tires = pd.read_excel('data_read/tyres.xlsx')
     print(df_tires)
+
+
+def string_with_found_word():
+    df = pd.read_excel('data_read/tyres.xlsx')
+    search_word = 'Winter'
+    result = df[df.apply(lambda row: row.astype(str).str.contains(search_word, case=False).any(), axis=1)]
+    print(result)
+
+
+def find_word_in_file():
+    workbook = openpyxl.load_workbook('data_read/tyres.xlsx')
+    sheet = workbook.active
+    search_word = 'SUV'
+    word_found = False
+
+    for row in sheet.iter_rows(values_only=True):
+        for cell in row:
+            if search_word in str(cell):
+                print(f'Слово {search_word} найдено в файле tyres.xls')
+                word_found = True
+                break
+        if word_found:
+            break
+    if not word_found:
+            print(f'Слово {search_word} не найдено')
+
+    # workbook.close()
+
 
 
 def find_diff():
@@ -21,4 +50,4 @@ def find_diff():
 
 
 if __name__ == '__main__':
-    read_file()
+    find_word_in_file()
